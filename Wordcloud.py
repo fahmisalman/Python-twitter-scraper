c198437da26c19a987ba622e5dc496454d67dec9
fahmisalman <fahmisalman@gmail.com>
2019-03-23 18:02:31 +0700

    Add mask

--- Wordcloud.py
+++ Wordcloud.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud, STOPWORDS
+from scipy.misc import imread
 
-
-df = pd.read_csv('fahmisalmann_tweets.csv')
+df = pd.read_csv('naufal_yai__tweets.csv')
 
 # join tweets to a single string
 words = ' '.join(df['text'])
@@ -15,11 +15,14 @@ no_urls_no_tags = " ".join([word for word in words.split()
                                 and word != 'RT'
                             ])
 
+twitter_mask = imread('./twitter_mask.png', flatten=True)
 wordcloud = WordCloud(
     stopwords=STOPWORDS,
-    background_color='black',
+    font_path='CabinSketch-Bold.ttf',
+    background_color='white',
     width=1800,
-    height=1400
+    height=1400,
+    mask=twitter_mask
     ).generate(no_urls_no_tags)
 
 plt.imshow(wordcloud)
