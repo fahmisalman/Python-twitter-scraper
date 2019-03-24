9b0dd47676f342ef05048cdc4c8cd104e5141a7c
fahmisalman <fahmisalman@gmail.com>
2019-03-24 16:51:56 +0700

    Organise file

--- Wordcloud.py
+++ Wordcloud.py
@@ -1,24 +1,24 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud, STOPWORDS
+from python_wordcloud import tweet_dumper as td
 from scipy.misc import imread
 
-df = pd.read_csv('naufal_yai__tweets.csv')
 
-# join tweets to a single string
-words = ' '.join(df['text'])
+df = td.fit('fahmisalmann')
 
-# remove URLs, RTs, and twitter handles
+words = ' '.join(df)
+
 no_urls_no_tags = " ".join([word for word in words.split()
                             if 'http' not in word
                                 and not word.startswith('@')
                                 and word != 'RT'
                             ])
 
-twitter_mask = imread('./twitter_mask.png', flatten=True)
+twitter_mask = imread('Assets/Mask/twitter_mask.png', flatten=True)
 wordcloud = WordCloud(
     stopwords=STOPWORDS,
-    font_path='CabinSketch-Bold.ttf',
+    font_path='Assets/Font/CabinSketch-Bold.ttf',
     background_color='white',
     width=1800,
     height=1400,
@@ -27,5 +27,5 @@ wordcloud = WordCloud(
 
 plt.imshow(wordcloud)
 plt.axis('off')
-plt.savefig('./my_twitter_wordcloud_1.png', dpi=300)
+plt.savefig('Output/my_twitter_wordcloud_1.png', dpi=300)
 plt.show()
---
+++ tweet_dumper.py
@@ -0,0 +1,32 @@
+from python_wordcloud.API_key import *
+import tweepy
+
+
+def get_all_tweets(screen_name):
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_key, access_secret)
+    api = tweepy.API(auth)
+
+    alltweets = []
+
+    new_tweets = api.user_timeline(screen_name=screen_name, count=280)
+    alltweets.extend(new_tweets)
+    oldest = alltweets[-1].id - 1
+
+    while len(new_tweets) > 0:
+        print("getting tweets before %s" % oldest)
+        new_tweets = api.user_timeline(screen_name=screen_name, count=200, max_id=oldest)
+        alltweets.extend(new_tweets)
+        oldest = alltweets[-1].id - 1
+
+        print("...%s tweets downloaded so far" % (len(alltweets)))
+
+    outtweets = [str(tweet.text.encode("utf-8")) for tweet in alltweets]
+    print(outtweets)
+
+    return outtweets
+
+
+def fit(user_account):
+    return get_all_tweets(user_account)
